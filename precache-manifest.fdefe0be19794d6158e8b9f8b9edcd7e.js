0abceea7bb4d91ee3a85b9dc82c064e758d0878b
Sharad Gawali <sharadgawali56@gmail.com>
2023-09-23 15:03:27 +0530

    Updates

---
+++ precache-manifest.fdefe0be19794d6158e8b9f8b9edcd7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e381591d01b3e0bea60",
+    "url": "/testing/static/css/main.efc17e7e.chunk.css"
+  },
+  {
+    "revision": "4e381591d01b3e0bea60",
+    "url": "/testing/static/js/main.4e381591.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/testing/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "faf732f1f21984600605",
+    "url": "/testing/static/js/2.faf732f1.chunk.js"
+  },
+  {
+    "revision": "b88266f0354bdcc95de1",
+    "url": "/testing/static/js/runtime~main.b88266f0.js"
+  },
+  {
+    "revision": "b3e8bbac684f37e59eb29fe53c1e2360",
+    "url": "/testing/index.html"
+  }
+];
